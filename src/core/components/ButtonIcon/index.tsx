33a2d9c6aecf141527d0c95893c15c1d1c6aed7d
rpźbass <ronaldop.tech@gmail.com>
2021-02-24 13:18:29 -0300

    Finish Home

---
+++ src/core/components/ButtonIcon/index.tsx
@@ -0,0 +1,22 @@
+import react from 'react';
+import './styles.css';
+
+
+
+type Props = {
+
+    buttonText : string;
+
+}
+
+export const ButtonIcon = ({buttonText}:Props) => (
+
+    <div>
+            <button className="button-comp">
+
+                <h5>{buttonText}</h5>
+
+            </button>
+    </div>
+
+);
---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+//import { Link } from 'react-router-dom';
+
+
+export const Navbar = () => (
+
+    <nav className="main-nav">
+        <div>
+            
+                <h4 className="main-home">Bootcamp DevSuperior</h4>    
+            
+        </div>         
+
+    </nav>
+
+);
